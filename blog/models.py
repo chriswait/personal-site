e195b46451ca722a03d9ba146674f782dc4bb209
chriswait <chriswait91@gmail.com>
2016-02-15 02:18:16 +1100

    adds models for blog and work
    adds adding apps to project settings
    adds migrations

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(null=True, max_length=50)
+    text = models.TextField(null=True)
+    preview = models.TextField(null=True)
+    url = models.URLField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+
+    def __unicode__(self):
+        return u'%s' % (self.title)
--- work/models.py
+++ work/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Work(models.Model):
+    title = models.CharField(null=True, max_length=50)
+    description = models.TextField(null=True)
+    url = models.URLField(null=True, blank=True)
+    date_from = models.DateField(null=True, blank=True)
+    date_to = models.DateField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __unicode__(self):
+        return u'%s' % (self.title)
