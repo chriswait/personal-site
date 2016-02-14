dc37e560ca010cccb3992cd3727f1cb22dfb30fd
chriswait <chriswait91@gmail.com>
2016-02-15 03:36:33 +1100

    services for fetching blog posts and work

--- app/blog/blog.js
+++ app/blog/blog.js
@@ -5,4 +5,26 @@ angular.module("personalSite")
         link: function(scope, element) {
         },
     };
-});
+})
+.factory('PostService', function($http, $q) {
+    var postsServiceInstance;
+    var data = {
+    };
+    var load_posts = function() {
+        var deferred = $q.defer();
+        $http.get("/api/posts")
+        .then(function(success_response) {
+            data.posts = success_response.data;
+            deferred.resolve(data.posts);
+        }, function(failure_response) {
+            deferred.reject();
+        });
+        return deferred.promise;
+    };
+    postsServiceInstance = {
+        data: data,
+        load_posts: load_posts,
+    };
+    return postsServiceInstance;
+})
+;
--- app/work/work.js
+++ app/work/work.js
@@ -5,4 +5,26 @@ angular.module("personalSite")
         link: function(scope, element) {
         },
     };
-});
+})
+.factory('WorkService', function($http, $q) {
+    var worksServiceInstance;
+    var data = {
+    };
+    var load_works = function() {
+        var deferred = $q.defer();
+        $http.get("/api/works")
+        .then(function(success_response) {
+            data.works = success_response.data;
+            deferred.resolve(data.works);
+        }, function(failure_response) {
+            deferred.reject();
+        });
+        return deferred.promise;
+    };
+    worksServiceInstance = {
+        data: data,
+        load_works: load_works,
+    };
+    return worksServiceInstance;
+})
+;
