203a164517e17e5bd534c3adc61143834cecb580
chriswait <chriswait91@gmail.com>
2016-02-15 03:44:45 +0000

    tweaks static files settings for cross-platform

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -103,6 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
@@ -110,7 +113,3 @@ REST_FRAMEWORK = {
     ],
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, ""),
-]
-print BASE_DIR
