75417dd024ec2bd933a36c7bea13289112cbba1c
chriswait <chriswait91@gmail.com>
2016-04-23 21:31:31 +1000

    moves templating into django on /blog and /work

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Post
 
 def index(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'blog/blog.html', {"posts": posts})
--- work/views.py
+++ work/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Work
 
 def index(request):
-    return render(request, 'work/work.html')
+    works = Work.objects.all()
+    return render(request, 'work/work.html', {"works": works})
