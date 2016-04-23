1c788f6040beb8b6052fa2148d203190776625b0
chriswait <chriswait91@gmail.com>
2016-04-23 22:54:02 +1000

    use django templating for index
    fixes indenting in blog template
    fixes urls for blog and post titles

--- personalsite/views.py
+++ personalsite/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+from work.models import Work
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all().order_by('-date')[:3]
+    works = Work.objects.filter(front_page=True)
+    return render(request, 'index.html', {"posts": posts, "works": works})
 
 def about(request):
     return render(request, 'about.html')
