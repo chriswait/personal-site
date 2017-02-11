528a58cae8fd82acf8c8a0c52b83ff0eb937b3fe
chriswait <chriswait91@gmail.com>
2017-02-11 17:08:43 +1100

    removes a whole bunch of unused angular stuff
    re-works gulpfile
    moves frontend into src/

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,44 @@
 var gulp = require('gulp');
-
-// PLUGINS
-var templateCache = require('gulp-angular-templatecache');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
-var stylish = require('jshint-stylish');
+var plumber = require('gulp-plumber');
+var eslint = require('gulp-eslint');
 
-// PATHS
-var app = 'app';
-var app_js = [app + '/**/*.js', "!" + app + '/templates.js'];
-var app_html = app + '/**/*.html';
-var app_scss = app + '/app.scss';
-var app_module_scss = app + '/**/*.scss';
+var jsPath = 'src/js/*.js';
+var sassPath = 'src/sass/*.scss';
+var distPath = 'static/dist/';
 
-var node_modules = "node_modules";
-var node_modules_js = [
-    node_modules + "/angular/angular.js",
-    node_modules + "/angular-animate/angular-animate.js",
-    node_modules + "/angular-aria/angular-aria.js",
-    node_modules + "/angular-material/angular-material.js",
-    node_modules + "/angular-sanitize/angular-sanitize.js",
-];
-var node_modules_css = [
-    node_modules + "/angular-material/angular-material.css"
-];
-
-var dist = 'static/dist';
-var vendor_js = "vendor.js";
-var vendor_css = "vendor.css";
-var js = "index.js";
-var css = "index.css";
-
-// TASKS
-
-// Vendor JS
-gulp.task('vendor-js', function() {
-    gulp.src(node_modules_js)
-    .pipe(concat(vendor_js))
-    .pipe(gulp.dest(dist));
-});
-// Vendor CSS
-gulp.task('vendor-css', function() {
-    gulp.src(node_modules_css)
-    .pipe(concat(vendor_css))
-    .pipe(gulp.dest(dist));
-});
-
-// Templates
-gulp.task('templates', function () {
-    gulp.src(app_html)
-    .pipe(templateCache({'standalone':1}))
-    .pipe(gulp.dest(dist));
-});
 // Lint
 gulp.task('lint', function() {
-    gulp.src(app_js)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    return gulp.src(jsPath)
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 // JS
 gulp.task('js', function() {
-    return gulp.src(app_js)
-        .pipe(concat(js))
-        .pipe(gulp.dest(dist));
+    return gulp.src(jsPath)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(distPath));
 });
 // SASS
 gulp.task('sass', function() {
-    gulp.src(app_scss)
+    gulp.src(sassPath)
+    .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
     }))
-    .pipe(concat(css))
-    .pipe(gulp.dest(dist));
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(distPath));
 });
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch(node_modules_js, ['vendor-js']);
-    gulp.watch(node_modules_css, ['vendor-css']);
-    gulp.watch(app_html, ['templates']);
-    gulp.watch(app_js, ['lint', 'js']);
-    gulp.watch(app_scss, ['sass']);
-    gulp.watch(app_module_scss, ['sass']);
+    gulp.watch(jsPath, ['lint', 'js']);
+    gulp.watch(sassPath, ['sass']);
 });
 
 // Default
-gulp.task('default', ['vendor-js', 'vendor-css', 'templates', 'lint', 'js', 'sass', 'watch']);
-gulp.task('build', ['vendor-js', 'vendor-css', 'templates', 'js', 'sass']);
+gulp.task('default', ['lint', 'js', 'sass', 'watch']);
