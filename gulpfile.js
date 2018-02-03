bb31e1ed55f2b64050bd498219a9996b3c56a588
chriswait <chriswait91@gmail.com>
2018-02-03 17:29:16 +1100

    adds browser-sync to gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,49 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var eslint = require('gulp-eslint');
+var browserSync = require('browser-sync').create();
 
 var jsPath = 'src/js/*.js';
 var sassPath = 'src/sass/*.scss';
 var distPath = 'static/dist/';
 
-// Lint
-gulp.task('lint', function() {
-    return gulp.src(jsPath)
-    .pipe(plumber())
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
 // JS
 gulp.task('js', function() {
-    return gulp.src(jsPath)
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest(distPath));
+  return gulp.src(jsPath)
+  .pipe(plumber())
+  .pipe(eslint())
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest(distPath));
 });
+gulp.task('js-watch', ['js'], function(done) {
+  browserSync.reload();
+  done();
+});
+
 // SASS
 gulp.task('sass', function() {
-    gulp.src(sassPath)
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-    }))
-    .pipe(concat('app.css'))
-    .pipe(gulp.dest(distPath));
+  gulp.src(sassPath)
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+  }))
+  .pipe(concat('app.css'))
+  .pipe(gulp.dest(distPath));
+});
+gulp.task('sass-watch', ['sass'], function(done) {
+  browserSync.reload();
+  done();
 });
 
-// Watch
-gulp.task('watch', function() {
-    gulp.watch(jsPath, ['lint', 'js']);
-    gulp.watch(sassPath, ['sass']);
-});
+// Build
+gulp.task('build', ['js', 'sass']);
 
-// Default
-gulp.task('default', ['lint', 'js', 'sass', 'watch']);
+// Serve
+gulp.task('default', ['build'], function() {
+  browserSync.init({
+    proxy: 'localhost:8000'
+  });
+  gulp.watch(jsPath, ['js-watch']);
+  gulp.watch(sassPath, ['sass-watch']);
+});
